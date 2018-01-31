e837275fde0624909e1217535126b363337b28f8
Japorized <johnson5756@live.com>
2018-01-31 18:30:15 -0500

    macos - ubersicht weather widget - workaround for faulty asset fetching

--- dotfiles/config/Übersicht/widgets/scripts/wttrWeather.sh
+++ dotfiles/config/Übersicht/widgets/scripts/wttrWeather.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/wget -qO ./cache/weather.png http://wttr.in/Waterloo_1pq_transparency\=175_lang\=en.png
+/usr/local/bin/wget -qO ./cache/weather.png http://wttr.in/Waterloo_2pq_transparency\=175.png
