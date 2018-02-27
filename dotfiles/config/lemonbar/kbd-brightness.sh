8c6cc27e4ba85f756f95b48bed6a7fc183a3bbc8
Japorized <johnson5756@live.com>
2018-02-27 16:17:21 -0500

    arch - lemonbar for kbd-brightness

---
+++ dotfiles/config/lemonbar/kbd-brightness.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+width="350"
+height="70"
+resolution="$(xrandr --nograb --current | awk '/\*/ {printf $1; exit}')"
+monitor_width="${resolution/x*}"
+monitor_height="${resolution#*x}"
+offset_width="$(( monitor_width / 2 - width / 2 ))"
+offset_height="$(( monitor_height / 2 + height ))"
+geometry="${width}x${height}+${offset_width}+${offset_height}"
+
+KBD_BRIGHTNESS(){
+    xbacklight -ctrl smc::kbd_backlight -get \
+    |SIZE=9 START="" END="" SEP="" CHAR1="" CHAR2=" " mkb
+}
+
+while true; do
+echo -e "%{l}%{F#fafafa}%{B#333333} \uf11c Brightness: $(KBD_BRIGHTNESS) %{F-}%{B-}" && sleep 1.5 && exit
+done | lemonbar -d -g $geometry -f "FontAwesome-18" -B "#333333"
+
