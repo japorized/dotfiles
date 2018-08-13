b3c363a9b2cee0bd1626bd0360d11f8dae50ffbd
Japorized <johnson5756@live.com>
2018-08-13 18:39:59 -0400

    regular updates

--- dotfiles/vim/templates/tex_default.sh
+++ dotfiles/vim/templates/tex_default.sh
@@ -1,24 +1,15 @@
-% Document Head
 \documentclass[notoc,notitlepage]{tufte-book}
 % \nonstopmode % uncomment to enable nonstopmode
-\setcounter{secnumdepth}{3}
-\setcounter{tocdepth}{3}
-\renewcommand{\baselinestretch}{1.1}
 
-\input{latex-classnotes-preamble.tex}
+\usepackage{classnotetitle}
 
 \title{Title}
 \author{Johnson Ng}
+\subtitle{Subtitle}
+\credentials{BMath (Hons), Pure Mathematics major, Actuarial Science Minor}
+\institution{University of Waterloo}
 
-% Header formatting
-\renewcommand{\chaptermark}[1]{\markboth{#1}{}}
-\renewcommand{\sectionmark}[1]{\markright{#1}}
-\makeatletter
-\pagestyle{fancy}
-\fancyhead{}
-\fancyhead[RO]{\textsl{\@title} \enspace \thepage}
-\fancyhead[LE]{\thepage \enspace \textsl{\leftmark \enspace - \enspace \rightmark}}
-\makeatother
+\input{latex-classnotes-preamble.tex}
 
 \begin{document}
 \hypersetup{pageanchor=false}
@@ -26,17 +17,26 @@
 \hypersetup{pageanchor=true}
 \tableofcontents
 
-\chapter*{List of Definitions}
+\chapter*{\faBook \enspace List of Definitions}
+\addcontentsline{toc}{chapter}{List of Definitions}
 \theoremlisttype{all}
 \listtheorems{defn}
 
-\chapter*{List of Theorems}
+\chapter*{\faCoffee \enspace List of Theorems}
+\addcontentsline{toc}{chapter}{List of Theorems}
 \theoremlisttype{allname}
 \listtheorems{axiom,lemma,thm,crly,propo}
 
 
+
+\appendix
+
+\backmatter
+
+\pagestyle{plain}
+
 \nobibliography*
-\bibliography{bibliography}
+\bibliography{references}
 
 \printindex
 
