1ebd5e38652de67f55b933dd3842fb8890ad7fa2
japorized <johnson.ng@snappymob.com>
2020-12-31 23:09:21 +0800

    Backup brewery for macOS

--- install/brew-install.sh
+++ install/brew-install.sh
@@ -1,68 +1,19 @@
 #!/bin/sh
 
-# brew taps
-brew tap phinze/cask
-brew tap caskroom/cask
-brew tap d12frosted/emacs-plus
+echo " === Setting up your macOS installation to be somewhat great. === "
+
+# Register taps
+echo " 🚰 Registering taps..."
+cat ./brewTaps.txt | xargs brew tap
 
 # brew installations
-brew install aria2
-brew install awk
-brew install buku
-brew install calc
-brew install coreutils
-brew install elinks
-brew install d12frosted/emacs-plus/emacs-plus
-brew install fftw
-brew install ghostscript
-brew install git
-brew install haskell-stack
-brew install htop
-brew install icdiff
-brew install id3v2
-brew install ispell
-brew install kyoto-cabinet
-brew install libsodium
-brew install mpc
-brew install mpd
-brew install mpdscribble
-brew install ncmpcpp
-brew install neofetch
-brew install nmap
-brew install node
-brew install opus-tools
-brew install opusfile
-brew install p7zip
-brew install perl
-brew install python
-brew install python3
-brew install reattach-to-user-namespace
-brew install ruby
-brew install tag
-brew install terminal-notifier
-brew install thefuck
-brew install tmux
-brew install utf8proc
-brew install vim
-brew install w3m
-brew install wget
-brew install youtube-dl
-brew install zsh
+echo " 🍺 Installing packages..."
+cat ./brewPackages.txt | xargs brew install
 
 # brew casks installations
-brew cask install amm
-brew cask install betterzipql
-brew cask install google-chrome
-brew cask install hammerspoon
-brew cask install handbrake
-brew cask install knockknock
-brew cask install lastfm
-brew cask install love
-brew cask install pomolectron
-brew cask install skim
-brew cask install spotify
-brew cask install vlc
-brew cask install xquartz
+echo " 🍺 Installing casks..."
+brew cask
+cat ./brewCask.txt | xargs brew install --cask
 
 # Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
